26dabe308092e6370fdfd9080170e93ebd878b73
Tracht <marija.trachtenberg@gmail.com>
2021-11-30 09:08:00 +0000

    Adds Search & Body Views, uses AndroidSafeArea

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.AndroidSafeArea}>
+          <View style={styles.search}>
+            <Text style={styles.title}>Search</Text>
+          </View> 
+
+          <View style={styles.body}>
+            <Text style={styles.title}>our blank canvas</Text>
+          </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  AndroidSafeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  search: {
+    padding: 16,
+    backgroundColor: "#5E84E2", // periwinkle
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  body: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#B6D0E2", // powerderBlue
+    alignItems: "center",
+    justifyContent: "center", 
+  },
+  title: {
+    color: "red",
+  }
 });
